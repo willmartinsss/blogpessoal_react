40a1521c771df1a67f10972261adc97558cea7f5
William Martins <134098387+willmartinsss@users.noreply.github.com>
2025-11-04 05:52:41 -0300

    Deployu do projeto pessoal

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://blogpessoal-nest-nmj3.onrender.com'
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const cadastrarUsuario = async (
